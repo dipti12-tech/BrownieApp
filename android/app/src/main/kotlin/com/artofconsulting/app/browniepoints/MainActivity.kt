dab3491ad67a42342bb7270035ecb4b5707ff0be
dipti p <dipti.pandilwar89@gmail.com>
2025-04-24 21:17:00 +0530

    Initial commit on dev branch

---
+++ android/app/src/main/kotlin/com/artofconsulting/app/browniepoints/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.artofconsulting.app.browniepoints
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:browniepoints/views/Splashscreen.dart';
+import 'package:browniepoints/views/WelcomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splashscreen(),
+      routes: {
+        '/views': (context) => WelcomScreen()
+      }
+    );
+  }
+
+
+}
+
+
+
