f1b3e4bc4c757e6038f824a6161db7307dc6cfef
aman <amanbiwal98@gmail.com>
2025-04-27 12:52:00 +0530

    Welcome and Login screen created.

---
+++ lib/utils/appstring.dart
@@ -0,0 +1,10 @@
+class AppStrings{
+static const String appName ="Brownie Pointss";
+static const String loginTitle = "Welcome Back!";
+static const String loginSubTitle = "Great to see you again";
+static const String emailaddress = "* Email Address";
+static const String password = "* Password";
+static const String btncontinue = "Continue";
+
+
+}
